3ed95632ce5af2ec0e1bdfa702b4c382eb81d5a4
Ivan-developer-cmngsoon <ivanwork003@gmail.com>
2024-12-09 00:43:30 -0300

    page menu listo, con menu circular para diseño UX

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
 })
-export class MenuPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class MenuPage {
+  toggleMenu() {
+    const menu = document.querySelector('.radial-menu');
+    menu?.classList.toggle('open');
+    const icon = document.querySelector('.main-btn ion-icon');
+    if (menu?.classList.contains('open')) {
+      icon?.setAttribute('name', 'close-outline');
+    } else {
+      icon?.setAttribute('name', 'menu-outline');
+    }
   }
-
+  
 }
