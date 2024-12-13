5119813be2e954facdee768fd2674940b936e7d1
Ivan-developer-cmngsoon <ivanwork003@gmail.com>
2024-12-13 12:52:42 -0300

    Empresas page.

--- src/app/empresas/empresas.page.ts
+++ src/app/empresas/empresas.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-empresas',
   templateUrl: './empresas.page.html',
   styleUrls: ['./empresas.page.scss'],
 })
-export class EmpresasPage implements OnInit {
+export class EmpresasPage {
 
-  constructor() { }
-
-  ngOnInit() {
+    constructor(private navCtrl: NavController) {}
+  
+    goBack() {
+      this.navCtrl.back(); // Navegar a la página anterior
+    }
+  
+    addToCart(product: string) {
+      console.log(`${product} añadido al carrito`);
+      alert(`${product} ha sido añadido al carrito.`);
+    }
   }
-
-}
