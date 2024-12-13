a8b5e2c21443b8612c157da52e698ca0c4ab0e12
Ivan-developer-cmngsoon <ivanwork003@gmail.com>
2024-12-13 13:28:02 -0300

    Page promociones especiales

--- src/app/promociones-esp/promociones-esp.page.ts
+++ src/app/promociones-esp/promociones-esp.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-promociones-esp',
   templateUrl: './promociones-esp.page.html',
   styleUrls: ['./promociones-esp.page.scss'],
 })
-export class PromocionesEspPage implements OnInit {
+export class PromocionesEspPage {
+  constructor(private navCtrl: NavController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  goBack() {
+    this.navCtrl.back(); // Navegar a la página anterior
   }
 
+  addToCart(product: string) {
+    console.log(`${product} añadido al carrito`);
+    alert(`${product} ha sido añadido al carrito.`);
+  }
 }
